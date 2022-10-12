ceb6babb7818f5f229e0bfa9de298e6b08448599
Stanislav Domrovskiy(Laptop) <stanislav_2010@icloud.com>
2022-10-12 20:38:25 +0500

    Fix hover in card

--- main.js
+++ main.js
@@ -1,28 +1,6 @@
 const logoBlick = document.querySelector('.header__logo-name');
 const socialMenuMore = document.querySelector('.header__social-mobile-more');
 const socialMenu = document.querySelector('.header__social-list');
-const hoverable = document.querySelectorAll('.hover-target');
-const hoverableItem = document.querySelectorAll('.hover-item');
-
-document.addEventListener('click', (e) => {
-    for (let i = 0; i < hoverable.length; i++) {
-        if (e.target != hoverableItem[i] && e.target != hoverable[i]) {
-            hoverableItem.forEach(element => {
-                element.classList.remove('active')
-            });
-        }
-    }
-    
-})
-
-for (let i = 0; i < hoverable.length; i++) {
-    hoverable[i].addEventListener('mouseover', () => {
-        hoverableItem[i].classList.add('active');
-        hoverable[i].addEventListener('mouseout', (e) => {
-                hoverableItem[i].classList.remove('active')
-        })
-    });
-}
 
 if (responsive(500)) {
     setInterval(() => {
